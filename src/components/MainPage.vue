4f63e0a62dda4eca0e1c690bb917ee010f45dd08
Yann Decelle <yann.decelle@edu.devinci.fr>
2021-03-27 01:31:06 +0100

    added add function

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container">
   <h1>Articles</h1>
-  <button v-on:click="addBlog(title)">Ajouter un article</button>
+  <input type="text" id="title" placeholder="Titre"><input type="text" id="content" placeholder="Contenu"><button v-on:click="addBlog(title)">Ajouter un article</button>
   <div class="blogCard" v-for="title in titles" :key="title.index">
     <div class="blogInfo">
       <h2 class="blogTitle">{{title.title}}</h2>
@@ -14,29 +14,50 @@
 </template>
 
 <script>
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+new Vuex.Store
+(
+  {
+    state:{},
+    mutations:{},
+    methods:{}
+  }
+)
 
 export default {
   name: 'MainPage',
   props: {
     msg: String
   },
-  
  methods:{
-    removeBlog: function(titles){
+    removeBlog: function(titles)
+    {
       let currentID = this.titles.indexOf(titles);
       var debugID = currentID +1;
       this.titles.splice(currentID, 1);
       console.log('Ligne numéro '+debugID+' a été supprimée.');
       console.log(debugID);
-    }   
+    },
+    addBlog: function(titles)
+    {
+      let currentID = this.titles.indexOf(titles);
+      var titleInput = document.getElementById('title').value;
+      var contentInput = document.getElementById('content').value;
+      this.titles.push({title: titleInput, content: contentInput});
+      console.log(currentID);
+      titleInput.value = '';
+      contentInput.value ='';
+    }     
   },
-    data() {
-    return {
-      titles: [
-        {title: "Hello", content: "Description, de l'article"},
-      ]
-    };
-  },
+    data: function()
+    {
+      return {titles: [{title: "Hello", content: "Description, de l'article"}]
+      };
+    },
 }
 </script>
 
